ed763373da53def40345a6955b5eb27960e316e0
John Doe <paytonanthony2020@gmail.com>
2021-06-10 11:12:07 -0600

    finished topic controller and components

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,58 @@
 class TopicsController < ApplicationController
+  before_action :set_sub
+  before_action :set_topic, only: [:show, :edit, :udpate, :destroy]
+  
   def index
+    @topics = @sub.topics
+    render component: 'Topics', props: { topics: @topics, sub: @sub }
   end
 
   def show
+    render component: 'Topic', props: { topic: @topic, sub: @sub }
   end
 
   def new
+    @topic = @sub.topics.new
+    render component: 'TopicNew', props: { topic: @topic, sub: @sub }
+  end
+
+  def create
+    @topic = @sub.topics.new(topic_params)
+    if @topic.save
+      redirect_to [@sub, @topic]
+    else
+      render component: 'TopicNew', props: { topic: @topic, sub: @sub }
+    end
   end
 
   def edit
+    render component: 'TopicEdit', props: { topic: @topic, sub: @sub }
   end
+
+  def udpate
+    if @topic.update(topic_params)
+      redirect_to [@sub, @topic]
+      # same as redirect_to sub_topic(@sub, topic)
+    else
+      render component: 'TopicEdit', props: { topic: @topic, sub: @sub }
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to sub_topics_path(@sub)
+  end
+
+  private
+    def set_sub
+      @sub = Sub.find(params[:sub_id])
+    end
+
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    def topic_params
+      params.require(:topic).permit(:name, :body)
+    end
 end
